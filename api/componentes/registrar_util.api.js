f18982fc6ff68720cbc34a788d27ac83d0ddca58
MaxwellAbarca <jchinchillaa@ucenfotec.ac.cr>
2019-03-24 01:32:10 -0600

    todo correcto

---
+++ api/componentes/registrar_util.api.js
@@ -0,0 +1,82 @@
+'use strict';
+const modelo_util = require('./regitstrar_util.model');
+module.exports.registrar = (req, res) => {
+    let nuevo_util = new modelo_util(
+        {
+            util1:req.body.util1,
+            util2:req.body.util2,
+            util3:req.body.util3,
+            util4:req.body.util4,
+            util5:req.body.util5,
+            util6:req.body.util6,
+            util7:req.body.util7,
+            util8:req.body.util8,
+            util9:req.body.util9,
+            util10:req.body.util10,
+
+            descripcion1:req.body.descripcion1,
+            descripcion2:req.body.descripcion2,
+            descripcion3:req.body.descripcion3,
+            descripcion4:req.body.descripcion4,
+            descripcion5:req.body.descripcion5,
+            descripcion6:req.body.descripcion6,
+            descripcion7:req.body.descripcion7,
+            descripcion8:req.body.descripcion8,
+            descripcion9:req.body.descripcion9,
+            descripcion10:req.body.descripcion10,
+           
+            cantidad1:req.body.cantidad1,
+            cantidad2:req.body.cantidad2,
+            cantidad3:req.body.cantidad3,
+            cantidad4:req.body.cantidad4,
+            cantidad5:req.body.cantidad5,
+            cantidad6:req.body.cantidad6,
+            cantidad7:req.body.cantidad7,
+            cantidad8:req.body.cantidad8,
+            cantidad9:req.body.cantidad9,
+            cantidad10:req.body.cantidad10
+
+        }
+    );
+    nuevo_util.save(function (error) {
+        if (error) {
+            res.json(
+                {
+                    success: false,
+                    msg: `no se registro el util ocurrió el siguiente error ${error}`
+                }
+            );
+        } else {
+            res.json(
+                {
+                    success: true,
+                    msg: `se envio correctamente el util`
+                }
+            );
+        }
+    });
+
+
+};
+module.exports.listar_utiles = (req, res) => {
+    modelo_util.find().then(
+        function (utiles) {
+            if (utiles.length > 0) {
+                res.json(
+                    {
+                        success: true,
+                        utiles: utiles
+                    }
+                )
+            } else {
+                res.json(
+                    {
+                        success: false,
+                        utiles: 'No se encontraron utiles'
+                    }
+                )
+            }
+        }
+    )
+
+};
---
+++ public/js/servicio_utiles.js
@@ -0,0 +1,82 @@
+'use strict';
+let listar_utiles = () => {
+    let listar_utiles = [];
+    let request = $.ajax({
+        url: "http://localhost:4000/api/listar_util",
+        method: "GET",
+        data: {
+
+        },
+        dataType: "json",
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        async: false
+    });
+
+    request.done(function (res) {
+        listar_utiles = res.utiles;
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+
+    });
+    return listar_utiles;
+};
+
+let registrar_utiles = (putil1, putil2, putil3, putil4, putil5,putil6,putil7,putil8,putil9,putil10,pdescrp1, pdescrp2, pdescrp3, pdescrp4,pdescrp5,pdescrp6,pdescrp7,pdescrp8,pdescrp9,pdescrp10,pcantdd1, pcantdd2, pcantdd3, pcantdd4,pcantdd5,pcantdd6,pcantdd7,pcantdd8,pcantdd9,pcantdd10) => {
+    let request = $.ajax({
+        url: "http://localhost:4000/api/registrar_util",
+        method: "POST",
+        data: {
+            util1: putil1,
+            util2: putil2,
+            util3: putil3,
+            util4: putil4,
+            util5: putil5,
+            util6: putil6,
+            util7: putil7,
+            util8: putil8,
+            util9: putil9,
+            util10: putil10,
+            
+            descripcion1: pdescrp1,
+            descripcion2: pdescrp2,
+            descripcion3: pdescrp3,
+            descripcion4: pdescrp4,
+            descripcion5: pdescrp5,
+            descripcion6: pdescrp6,
+            descripcion7: pdescrp7,
+            descripcion8: pdescrp8,
+            descripcion9: pdescrp9,
+            descripcion10: pdescrp10,
+           
+            cantidad1: pcantdd1,
+            cantidad2: pcantdd2,
+            cantidad3: pcantdd3,
+            cantidad4: pcantdd4,
+            cantidad5: pcantdd5,
+            cantidad6: pcantdd6,
+            cantidad7: pcantdd7,
+            cantidad8: pcantdd8,
+            cantidad9: pcantdd9,
+            cantidad10: pcantdd10
+            
+
+        },
+        dataType: "json",
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+
+    });
+
+    request.done(function (res) {
+        swal.fire(
+            {
+                type: 'success',
+                title: 'lista de utiles enviada de manera correcta'
+            });
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+
+    });
+
+};
