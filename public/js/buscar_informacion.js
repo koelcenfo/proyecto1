ea6215036866c861dba78be4bb0cd32df94e7c4d
lurizarm <[lurizarm@ucenfotec.ac.cr]>
2019-03-31 05:04:33 -0600

    Se agrego el buscar los centros educativos con su respectivo perfil y su estilo, ademas se grego estilos a la pagina del perfil

--- public/js/buscar_informacion.js
+++ public/js/buscar_informacion.js
@@ -1,33 +1,46 @@
 'use strict';
 
-const tabla = document.querySelector('#tbl_instituciones tbody');
-
-
+const lista = document.querySelector('#lista_instituciones');
+const boton_perfil = document.getElementsByClassName('link');
 let mostrar_datos = () => {
     
     let institucion = listar_institucion();
+    sessionStorage.removeItem('id_perfil');
+    for (let i = 0; i < institucion.length; i++) {
+        
+        var nombre = document.createElement('h2');
+        nombre.innerHTML = institucion[i]['institucion_nombre'];
+        lista.appendChild(nombre);
+        const perfil = document.createElement('a');
+        perfil.classList.add('link');
+        const linkText = document.createTextNode("Visitar");
+        perfil.appendChild(linkText);
+        perfil.title = institucion[i]['institucion_nombre'];
+        perfil.href = 'http://localhost:3000/public/perfil.html?/'+institucion[i]['institucion_id'];
+        lista.appendChild(perfil);
+    }
 
-    for(let i = 0; i < institucion.length; i++){
-
-        let fila = tabla.insertRow();
-
-        fila.insertCell().innerHTML = institucion[i]['institucion_nombre'];
-        fila.insertCell().innerHTML = institucion[i]['institucion_provincia'];
-        fila.insertCell().innerHTML = institucion[i]['institucion_tipo'];
-        fila.insertCell().innerHTML = document.createElement('a');
-        tabla.rows[i].style.display = 'none';
-    };          
+ 
 };
 
 function Buscar() {
-    var tabla = document.getElementById('tbl_instituciones');
+
+    var tabla = document.getElementById('#lista_instituciones');
+
     var busqueda = document.getElementById('txtBusqueda').value.toLowerCase();
+
     var cellsOfRow="";
+
     var found=false;
+
     var compareWith="";
+
     for (var i = 1; i < tabla.rows.length; i++) {
+
         cellsOfRow = tabla.rows[i].getElementsByTagName('td');
+
         found = false;
+
         for (var j = 0; j < cellsOfRow.length && !found; j++) { compareWith = cellsOfRow[j].innerHTML.toLowerCase(); if (busqueda.length == 0 || (compareWith.indexOf(busqueda) > -1))
             {
                 found = true;
--- public/js/controlador_barra_navegacion.js
+++ public/js/controlador_barra_navegacion.js
@@ -4,30 +4,41 @@ const enlaces = document.querySelectorAll('.barra_navegacion a');
 let conectado = sessionStorage.getItem('conectado');
 let tipo_usuario = sessionStorage.getItem('tipo_usuario');
 
+function cerrar_sesion(){
+    sessionStorage.removeItem('conectado');
+    sessionStorage.removeItem('tipo_usuario');
+    sessionStorage.removeItem('id_usuario');
+    sessionStorage.removeItem('id_perfil');
+    window.location.href('http://localhost:3000/public/index.html');
+}
+
 if(conectado){
 
 
     switch(tipo_usuario){
         case 'Padre':
-        enlaces[0].classList.add('ocultar');
-        enlaces[1].classList.add('ocultar');
-        enlaces[2].classList.add('ocultar');
-        enlaces[3].classList.add('mostrar');
-        enlaces[4].classList.add('mostrar');
+        enlaces[0].classList.add('ocultar');/**Registro Padre */
+        enlaces[1].classList.add('ocultar');/**Registro Institución*/
+        enlaces[2].classList.add('ocultar');/**Iniciar Sesión */
+        enlaces[3].classList.add('mostrar');/**Perfil */
+        enlaces[4].classList.add('mostrar');/**Buscar Centro Educativo */
+        enlaces[5].classList.add('mostrar');/**Cerrar Sesion */
         break;
 
         case 'Institucion':
-            enlaces[0].classList.add('ocultar');
-            enlaces[1].classList.add('ocultar');
-            enlaces[2].classList.add('ocultar');
-            enlaces[3].classList.add('mostrar');
-            enlaces[4].classList.add('ocultar');
+            enlaces[0].classList.add('ocultar');/**Registro Padre */
+            enlaces[1].classList.add('ocultar');/**Registro Institución*/
+            enlaces[2].classList.add('ocultar');/**Iniciar Sesión */
+            enlaces[3].classList.add('mostrar');/**Perfil */
+            enlaces[4].classList.add('ocultar');/**Buscar Centro Educativo */
+            enlaces[5].classList.add('mostrar');/**Cerrar Sesion */
         break;
     }
 }else{
-    enlaces[0].classList.add('mostrar');
-    enlaces[1].classList.add('mostrar');
-    enlaces[2].classList.add('mostrar');
-    enlaces[3].classList.add('ocultar');
-    enlaces[4].classList.add('ocultar');
+    enlaces[0].classList.add('mostrar');/**Registro Padre */
+    enlaces[1].classList.add('mostrar');/**Registro Institución*/
+    enlaces[2].classList.add('mostrar');/**Iniciar Sesión */
+    enlaces[3].classList.add('ocultar');/**Perfil */
+    enlaces[4].classList.add('ocultar');/**Buscar Centro Educativo */
+    enlaces[5].classList.add('ocultar');/**Cerrar Sesion */
 }  
